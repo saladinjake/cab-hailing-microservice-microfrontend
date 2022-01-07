95455736cfa5160f9cd3cee1c12ea6cfb618cff6
saladinjake <juwavictor@gmail.com>
2022-01-07 15:00:00 +0100

    feat: implement API Gateway using express and http-proxy-middleware

---
+++ backend/api-gateway/src/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('combined'));
+
+// Health check
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'API Gateway is running' });
+});
+
+// Proxy rules
+app.use('/api/users', createProxyMiddleware({ target: process.env.USER_SERVICE_URL || 'http://localhost:5001', changeOrigin: true }));
+app.use('/api/rides', createProxyMiddleware({ target: process.env.RIDE_SERVICE_URL || 'http://localhost:5002', changeOrigin: true }));
+app.use('/api/notifications', createProxyMiddleware({ target: process.env.NOTIFICATION_SERVICE_URL || 'http://localhost:5003', changeOrigin: true }));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`API Gateway is running on port ${PORT}`);
+});
