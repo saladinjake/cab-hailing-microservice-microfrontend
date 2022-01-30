90468302a5d07e02ac7d065198ac3817a37d7a62
saladinjake <juwavictor@gmail.com>
2022-01-30 22:20:00 +0100

    fix: add Kafka healthcheck, restart policies and resilient retry logic to resolve startup race

--- backend/notification-service/src/events/kafkaClient.js
+++ backend/notification-service/src/events/kafkaClient.js
@@ -2,34 +2,36 @@ const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
   clientId: 'notification-service',
-  brokers: [process.env.KAFKA_BROKER || 'localhost:9192']
+  brokers: [process.env.KAFKA_BROKER || 'localhost:9192'],
+  retry: { retries: 20, initialRetryTime: 3000 },
 });
 
 const consumer = kafka.consumer({ groupId: 'notification-group' });
 
 const connectConsumer = async (io) => {
-  try {
-    await consumer.connect();
-    console.log('Connected to Kafka consumer');
-    
-    await consumer.subscribe({ topic: 'ride-events', fromBeginning: true });
-    
-    await consumer.run({
-      eachMessage: async ({ topic, partition, message }) => {
-        const event = JSON.parse(message.value.toString());
-        console.log(`Received event: ${event.type}`);
-        
-        // Broadcast to relevant rooms/users via Socket.io
-        if (event.type === 'RIDE_REQUESTED') {
-          io.emit('newRideRequest', event.payload);
-        } else if (event.type === 'RIDE_ACCEPTED' || event.type === 'RIDE_UPDATED') {
-          io.to(`ride:${event.payload.rideId}`).emit('rideUpdate', event.payload);
-        }
-      },
-    });
-  } catch (error) {
-    console.error('Error connecting to Kafka:', error);
-  }
+  const attempt = async (retries = 0) => {
+    try {
+      await consumer.connect();
+      console.log('Connected to Kafka consumer');
+      await consumer.subscribe({ topic: 'ride-events', fromBeginning: false });
+      await consumer.run({
+        eachMessage: async ({ message }) => {
+          const event = JSON.parse(message.value.toString());
+          console.log(`Kafka event received: ${event.type}`);
+          if (event.type === 'RIDE_REQUESTED') {
+            io.emit('newRideRequest', event.payload);
+          } else if (event.type === 'RIDE_ACCEPTED' || event.type === 'RIDE_UPDATED') {
+            io.to(`ride:${event.payload.rideId}`).emit('rideUpdate', event.payload);
+          }
+        },
+      });
+    } catch (error) {
+      const wait = Math.min(5000 * (retries + 1), 30000);
+      console.error(`Kafka connect failed (attempt ${retries + 1}), retrying in ${wait / 1000}s...`);
+      setTimeout(() => attempt(retries + 1), wait);
+    }
+  };
+  attempt();
 };
 
 module.exports = { connectConsumer };
--- backend/ride-service/src/events/kafkaClient.js
+++ backend/ride-service/src/events/kafkaClient.js
@@ -2,28 +2,28 @@ const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
   clientId: 'ride-service',
-  brokers: [process.env.KAFKA_BROKER || 'localhost:9192']
+  brokers: [process.env.KAFKA_BROKER || 'localhost:9192'],
+  retry: { retries: 20, initialRetryTime: 3000 },
 });
 
 const producer = kafka.producer();
 
-const connectProducer = async () => {
+const connectProducer = async (retries = 0) => {
   try {
     await producer.connect();
     console.log('Connected to Kafka producer');
   } catch (error) {
-    console.error('Error connecting to Kafka:', error);
+    const wait = Math.min(5000 * (retries + 1), 30000);
+    console.error(`Kafka producer connect failed (attempt ${retries + 1}), retrying in ${wait / 1000}s...`);
+    setTimeout(() => connectProducer(retries + 1), wait);
   }
 };
 
 const sendRideEvent = async (topic, eventPayload) => {
   try {
-    await producer.send({
-      topic,
-      messages: [{ value: JSON.stringify(eventPayload) }],
-    });
+    await producer.send({ topic, messages: [{ value: JSON.stringify(eventPayload) }] });
   } catch (error) {
-    console.error(`Error sending message to topic ${topic}:`, error);
+    console.error(`Failed to send Kafka event on topic ${topic}:`, error.message);
   }
 };
 
